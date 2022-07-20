728519e3f419b618541415b708e9297a9b64dc07
Prateek-Netling <109517264+Prateek-Netling@users.noreply.github.com>
2022-07-20 13:01:28 +0530

    corporate page js

--- assets/main.js
+++ assets/main.js
@@ -32,11 +32,36 @@ $(document).ready(function() {
     }
   });
 
+$('.carousel2').owlCarousel({
+  center: true,
+  items:3,
+  loop:true,
+  margin:30,
+  nav:false,
+  dots: false,
+  autoplay: true,
+  slideTransition: 'linear',
+  autoplayTimeout: 6000,
+  autoplaySpeed: 6000,
+  autoplayHoverPause: false,
+  responsive:{
+    0: {
+      items:2
+    },768:{
+        items:4,
+    },992:{
+      margin:24,
+      items:6,
+    }
+  }
+});
+
   if (/Edge\/\d./i.test(navigator.userAgent)){
     jQuery('html').addClass('ie');
   };
 
-
+  $( "#datepicker" ).datepicker();
+  
 });
 var s = skrollr.init();
 $(window).load(function() {
